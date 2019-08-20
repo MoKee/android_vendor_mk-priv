671b7c536da3a2e8dbf2d6c0c9624b00f32500be
Martincz Gao <martincz@mokeedev.com>
2019-08-20 23:24:24 +0800

    mk-priv: Derp
    Change-Id: Ie9ee9410e548ebfe9023bc8af60557e53e15b4d4

--- prebuilt/google/app/Gboard/Android.mk
+++ prebuilt/google/app/Gboard/Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := LatinIMEGooglePrebuilt
+LOCAL_MODULE := Gboard
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE_CLASS := APPS
--- prebuilt/google/app/Pinyin/Android.mk
+++ prebuilt/google/app/Pinyin/Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := GooglePinyinIME
+LOCAL_MODULE := Pinyin
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE_CLASS := APPS
