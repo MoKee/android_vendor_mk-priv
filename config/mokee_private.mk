0bb777bdc55886470380b56ff3489bd7c5d2171b
Martincz Gao <martincz@mokeedev.com>
2019-09-10 12:27:00 +0800

    mk-priv: rename path
    Change-Id: I4da8601f7bfb42161302b55754ddc4588b3535da

--- config/mokee_private.mk
+++ config/mokee_private.mk
@@ -27,25 +27,25 @@ PRODUCT_PACKAGES += \
 # Use all private libraries
 ifeq ($(MK_CPU_ABI),arm64-v8a)
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*.so,vendor/mk-priv/prebuilt/mokee/lib/$(MK_CPU_ABI),system/lib64) \
-    $(call find-copy-subdir-files,*.so,vendor/mk-priv/prebuilt/mokee/lib/armeabi-v7a,system/lib)
+    $(call find-copy-subdir-files,*.so,vendor/mokee-priv/prebuilt/mokee/lib/$(MK_CPU_ABI),system/lib64) \
+    $(call find-copy-subdir-files,*.so,vendor/mokee-priv/prebuilt/mokee/lib/armeabi-v7a,system/lib)
 else ifeq ($(MK_CPU_ABI),x86_64)
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*.so,vendor/mk-priv/prebuilt/mokee/lib/$(MK_CPU_ABI),system/lib64) \
-    $(call find-copy-subdir-files,*.so,vendor/mk-priv/prebuilt/mokee/lib/x86,system/lib)
+    $(call find-copy-subdir-files,*.so,vendor/mokee-priv/prebuilt/mokee/lib/$(MK_CPU_ABI),system/lib64) \
+    $(call find-copy-subdir-files,*.so,vendor/mokee-priv/prebuilt/mokee/lib/x86,system/lib)
 else
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*.so,vendor/mk-priv/prebuilt/mokee/lib/$(MK_CPU_ABI),system/lib)
+    $(call find-copy-subdir-files,*.so,vendor/mokee-priv/prebuilt/mokee/lib/$(MK_CPU_ABI),system/lib)
 endif
 
 # Offline phone location database
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,mokee-phonelocation.dat,vendor/mk-priv/prebuilt/mokee/media/location,system/media/location)
+    $(call find-copy-subdir-files,mokee-phonelocation.dat,vendor/mokee-priv/prebuilt/mokee/media/location,system/media/location)
 
 # Optimize for low-end devices
 ifneq ($(filter aries armani dior find7 gucci m8d m8 wt88047,$(MK_BUILD)),)
 # Include MK audio files
-include vendor/mk/config/mokee_audio_mini.mk
+include vendor/mokee/config/mokee_audio_mini.mk
 TARGET_BOOTANIMATION_HALF_RES := true
 USE_REDUCED_CJK_FONT_WEIGHTS := true
 # Speed profile services and wifi-service to reduce RAM and storage.
@@ -56,7 +56,7 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_DEFAULT_INPUT_METHOD := Pinyin
 else
 # Include MK audio files
-include vendor/mk/config/mokee_audio.mk
+include vendor/mokee/config/mokee_audio.mk
 PRODUCT_PACKAGES += \
     vim
 ifeq ($(filter armeabi armeabi-v7a arm64-v8a,$(MK_CPU_ABI)),)
@@ -74,10 +74,10 @@ PRODUCT_PACKAGES += \
 ifneq (${PRODUCT_DEFAULT_MOKEE_CERTIFICATE},)
 PRODUCT_DEFAULT_DEV_CERTIFICATE := ${PRODUCT_DEFAULT_MOKEE_CERTIFICATE}/releasekey
 else
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/mk-priv/build/target/product/security/releasekey
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/mokee-priv/build/target/product/security/releasekey
 endif
 
 # Use MoKee Java Source Overlays
 ifneq ($(filter PREMIUM,$(MK_BUILDTYPE)),)
-JAVA_SOURCE_OVERLAYS := framework|vendor/mk-priv/overlay/premium/frameworks/base|**/*.java
+JAVA_SOURCE_OVERLAYS := framework|vendor/mokee-priv/overlay/premium/frameworks/base|**/*.java
 endif
