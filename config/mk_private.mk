77d8b35784faf906da10f9d74f6f8e5390ad63ba
Martincz Gao <martincz@mokeedev.com>
2019-11-01 20:05:20 +0800

    mk-priv: Improve inputmethod config
    Change-Id: Ic5f7fdc8da2877ff8d566d3956749c553b3b710f

--- config/mk_private.mk
+++ config/mk_private.mk
@@ -40,19 +40,11 @@ endif
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,mokee-phonelocation.dat,vendor/mk-priv/prebuilt/mokee/media/location,system/media/location)
 
+# Optimize for low-end devices
 ifneq ($(filter armani condor deb dior falcon find7 flo gucci i9100 mako tomato wt88047,$(MK_BUILD)),)
 SMALL_BOARD_SYSTEMIMAGE_PARTITION := true
 TARGET_BOOTANIMATION_HALF_RES := true
 USE_REDUCED_CJK_FONT_WEIGHTS := true
-endif
-
-# Default input method apps
-ifeq ($(filter armeabi armeabi-v7a arm64-v8a,$(MK_CPU_ABI)),)
-PRODUCT_PACKAGES += \
-    LatinIME
-else
-PRODUCT_PACKAGES += \
-    Gboard
 endif
 
 # Disable dex-preopt of some devices to save space.
@@ -60,17 +52,27 @@ ifeq ($(SMALL_BOARD_SYSTEMIMAGE_PARTITION),true)
 # Include MK audio files
 include vendor/mk/config/mk_audio_mini.mk
 WITH_DEXPREOPT := false
+PRODUCT_DEFAULT_INPUT_METHOD := Pinyin
 else
 # Include MK audio files
 include vendor/mk/config/mk_audio.mk
 PRODUCT_PACKAGES += \
     vim
+ifeq ($(filter armeabi armeabi-v7a arm64-v8a,$(MK_CPU_ABI)),)
+PRODUCT_DEFAULT_INPUT_METHOD := LatinIME
+else
+PRODUCT_DEFAULT_INPUT_METHOD := Gboard
+endif
 endif
 
 # Disable dex-preopt of some devices to fix compile.
 ifneq ($(filter h811 h815 h830 h850,$(MK_BUILD)),)
 WITH_DEXPREOPT := false
 endif
+
+# Default input method apps
+PRODUCT_PACKAGES += \
+    $(PRODUCT_DEFAULT_INPUT_METHOD)
 
 # Use MoKee build keys
 ifneq ($(filter HISTORY NIGHTLY RELEASE,$(MK_BUILDTYPE)),)
