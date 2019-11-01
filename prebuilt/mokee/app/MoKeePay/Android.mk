9276c24a9c560d12d40f033b8d997ed7707c9c86
Martincz Gao <martincz@mokeedev.com>
2019-11-01 19:35:45 +0800

    mk-priv: Update Android.mk for MoKeePay
    Change-Id: Ibe7a74fa47ba08de111284722cf0af65bff38077

--- prebuilt/mokee/app/MoKeePay/Android.mk
+++ prebuilt/mokee/app/MoKeePay/Android.mk
@@ -1,17 +1,18 @@
 #
-# Copyright (C) 2016 The MoKee Open Source Project
+# Copyright (C) 2016-2019 The MoKee Open Source Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 LOCAL_PATH:= $(call my-dir)
@@ -22,11 +23,13 @@ LOCAL_MODULE := MoKeePay
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_SRC_FILES := MoKeePay.apk
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 
 LOCAL_MODULE_CLASS := APPS
 LOCAL_BUILT_MODULE_STEM := package.apk
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
+
+LOCAL_DEX_PREOPT := nostripping
 
 include $(BUILD_PREBUILT)
