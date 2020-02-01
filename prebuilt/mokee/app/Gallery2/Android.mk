117fa0db54e3eb323700da81afd442eb9a324900
Martincz Gao <martincz@mokeedev.com>
2020-02-01 21:58:27 +0800

    mk-priv: Update Gallery2 to v6.12.0
    Change-Id: I0ff1f398c28d8ad643d948f2d763878fece877d0

--- prebuilt/mokee/app/Gallery2/Android.mk
+++ prebuilt/mokee/app/Gallery2/Android.mk
@@ -27,28 +27,66 @@ LOCAL_BUILT_MODULE_STEM := package.apk
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 
 LOCAL_PREBUILT_JNI_LIBS_arm := \
-    @lib/armeabi-v7a/libNativeImageProcessor.so \
-    @lib/armeabi-v7a/libpano_video_renderer.so \
     @lib/armeabi-v7a/libpanorenderer.so \
-    @lib/armeabi-v7a/libpl_droidsonroids_gif.so
+    @lib/armeabi-v7a/libpano_video_renderer.so \
+    @lib/armeabi-v7a/libpl_droidsonroids_gif.so \
+    @lib/armeabi-v7a/librsjni_androidx.so \
+    @lib/armeabi-v7a/librsjni.so \
+    @lib/armeabi-v7a/librs.jpeg_v6_5_0.so \
+    @lib/armeabi-v7a/librs.render_3d_lut_v6_5_0.so \
+    @lib/armeabi-v7a/librs.render_adjust_v6_5_1.so \
+    @lib/armeabi-v7a/librs.render_blur_v6_5_0.so \
+    @lib/armeabi-v7a/librs.render_clarity_v6_5_0.so \
+    @lib/armeabi-v7a/librs.render_duotone_v6_5_0.so \
+    @lib/armeabi-v7a/librs.render_sharpness_v6_5_1.so \
+    @lib/armeabi-v7a/librs.split_v6_5_0.so \
+    @lib/armeabi-v7a/libRSSupport.so
 
 LOCAL_PREBUILT_JNI_LIBS_arm64 := \
-    @lib/arm64-v8a/libNativeImageProcessor.so \
-    @lib/arm64-v8a/libpano_video_renderer.so \
     @lib/arm64-v8a/libpanorenderer.so \
-    @lib/arm64-v8a/libpl_droidsonroids_gif.so
+    @lib/arm64-v8a/libpano_video_renderer.so \
+    @lib/arm64-v8a/libpl_droidsonroids_gif.so \
+    @lib/arm64-v8a/librsjni_androidx.so \
+    @lib/arm64-v8a/librsjni.so \
+    @lib/arm64-v8a/librs.jpeg_v6_5_0.so \
+    @lib/arm64-v8a/librs.render_3d_lut_v6_5_0.so \
+    @lib/arm64-v8a/librs.render_adjust_v6_5_1.so \
+    @lib/arm64-v8a/librs.render_blur_v6_5_0.so \
+    @lib/arm64-v8a/librs.render_clarity_v6_5_0.so \
+    @lib/arm64-v8a/librs.render_duotone_v6_5_0.so \
+    @lib/arm64-v8a/librs.render_sharpness_v6_5_1.so \
+    @lib/arm64-v8a/librs.split_v6_5_0.so \
+    @lib/arm64-v8a/libRSSupport.so
 
 LOCAL_PREBUILT_JNI_LIBS_x86 := \
-    @lib/x86/libNativeImageProcessor.so \
-    @lib/x86/libpano_video_renderer.so \
     @lib/x86/libpanorenderer.so \
-    @lib/x86/libpl_droidsonroids_gif.so
+    @lib/x86/libpano_video_renderer.so \
+    @lib/x86/libpl_droidsonroids_gif.so \
+    @lib/x86/librsjni_androidx.so \
+    @lib/x86/librsjni.so \
+    @lib/x86/librs.jpeg_v6_5_0.so \
+    @lib/x86/librs.render_3d_lut_v6_5_0.so \
+    @lib/x86/librs.render_adjust_v6_5_1.so \
+    @lib/x86/librs.render_blur_v6_5_0.so \
+    @lib/x86/librs.render_clarity_v6_5_0.so \
+    @lib/x86/librs.render_duotone_v6_5_0.so \
+    @lib/x86/librs.render_sharpness_v6_5_1.so \
+    @lib/x86/librs.split_v6_5_0.so \
+    @lib/x86/libRSSupport.so
 
 LOCAL_PREBUILT_JNI_LIBS_x86_64 := \
-    @lib/x86_64/libNativeImageProcessor.so \
-    @lib/x86_64/libpano_video_renderer.so \
-    @lib/x86_64/libpanorenderer.so \
-    @lib/x86_64/libpl_droidsonroids_gif.so
+    @lib/x86_64/libpl_droidsonroids_gif.so \
+    @lib/x86_64/librsjni_androidx.so \
+    @lib/x86_64/librsjni.so \
+    @lib/x86_64/librs.jpeg_v6_5_0.so \
+    @lib/x86_64/librs.render_3d_lut_v6_5_0.so \
+    @lib/x86_64/librs.render_adjust_v6_5_1.so \
+    @lib/x86_64/librs.render_blur_v6_5_0.so \
+    @lib/x86_64/librs.render_clarity_v6_5_0.so \
+    @lib/x86_64/librs.render_duotone_v6_5_0.so \
+    @lib/x86_64/librs.render_sharpness_v6_5_1.so \
+    @lib/x86_64/librs.split_v6_5_0.so \
+    @lib/x86_64/libRSSupport.so
 
 LOCAL_CERTIFICATE := releasekey
 
